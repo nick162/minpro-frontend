4ba1298644119d7dff2420b705484d9b47cbd7a0
nick162 <azwar.hanik@gmail.com>
2025-05-03 19:36:30 +0700

    feat: add etc

---
+++ src/hooks/api/ticket/useGetTickets.ts
@@ -0,0 +1,21 @@
+// hooks/api/Ticket/useGetTickets.ts
+import { axiosInstance } from "@/lib/axios";
+import { useQuery } from "@tanstack/react-query";
+import { Ticket } from "@/types/ticket";
+
+export const fetchTicketsByEventId = async (
+  eventId: number
+): Promise<Ticket[]> => {
+  const { data } = await axiosInstance.get<Ticket[]>(
+    `/event/${eventId}/tickets`
+  );
+  return data;
+};
+
+export const useGetTickets = (eventId: number) => {
+  return useQuery({
+    queryKey: ["event-tickets", eventId],
+    queryFn: () => fetchTicketsByEventId(eventId),
+    enabled: !!eventId,
+  });
+};
